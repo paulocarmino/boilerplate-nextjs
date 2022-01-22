e9cd864b3169bb8834c066482c790030689fda0d
Paulo Carmino <contato@paulocarmino.com>
2022-01-22 18:57:28 -0300

    update boilerplate

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,19 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
+    '!src/pages/**/*.tsx',
     '!src/**/stories.tsx',
     '!src/pages/_app.tsx',
     '!src/pages/index.tsx'
   ],
+  coveragePathIgnorePatterns: ['<rootDir>/src/utils'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '~/(.*)': '<rootDir>/$1'
   }
 }
